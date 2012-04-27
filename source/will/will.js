89dda5b940af75f0377cf7b7a53305b099b8581f
Marcelo Manzan <manzan@gmail.com>
2012-04-27 00:21:50 -0300

    New version (v1.3.0)

--- source/will/will.js
+++ source/will/will.js
@@ -1,11 +1,11 @@
 /*!
- * WillJS JavaScript Library v1.2.2
+ * WillJS JavaScript Library v1.3.0
  * http://github.com/kawamanza/will.js
  *
  * Copyright 2011-2012, Marcelo Manzan
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
- * Date: Sun Apr 01 17:09:14 2012 -0300
+ * Date: Fry Apr 27 00:16:08 2012 -0300
  */
 
 (function (window, globalName, undefined) {
@@ -760,7 +760,7 @@
     function Defaults() {}
     extend(Defaults.prototype, {
         "mode": basicApi.modes.DEV,
-        "version": "1.2.2",
+        "version": "1.3.0",
         "addDomain": function (domainName, urlPrefix, asJS, mode) {
             this.domains[domainName] = {format:(isString(asJS) ? asJS : asJS ? "js" : "json"), domain: urlPrefix + (/\/$/.test(urlPrefix) ? "" : "/"), mode: mode};
         },
