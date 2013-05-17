37acb4587c5f785fbd37810aadd1ca594a7b13c2
Marcelo Manzan <manzan@gmail.com>
2013-05-17 00:59:41 -0300

    changing willjs configuration to load components

--- demo/js/will/config.js
+++ demo/js/will/config.js
@@ -1,7 +1,7 @@
 will.configure(function (config) {
-    will.u.extend(config.domains.local, {
-        format: "js"
-      , mode: will.modes.DEV
-    });
-    config.debug = function (msg) { console.log(msg); };
+    config.debug = function (msg) {
+        if ( window.console && console.log ) {
+            console.log(msg);
+        }
+    };
 });
